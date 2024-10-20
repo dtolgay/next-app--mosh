2e20eefd941c19bb0a94a4362833271895150d78
Doga Tolgay <56132931+dtolgay@users.noreply.github.com>
2024-10-20 16:14:53 -0400

    first commit

---
+++ app/components/AddToCard.tsx
@@ -0,0 +1,10 @@
+'use client'
+import React from 'react'
+
+const AddToCard = () => {
+    return (
+        <button onClick={() => console.log('Clicked')}>Add 2 card</button>
+    )
+}
+
+export default AddToCard
